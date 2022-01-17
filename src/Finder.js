3ae46e4ffe0153a251ebfcffc819c04ca1a36158
Kevin Bird <kevin.bird@mariner-usa.com>
2022-01-16 21:20:15 -0600

    LearnMore Component done

---
+++ src/Finder.js
@@ -0,0 +1,30 @@
+import './Finder.css';
+import LearnMore from './components/LearnMore'
+
+function Finder() {
+  return (
+    <div className="Finder container-fluid">
+      <main>
+        <div className='px-4 py-5 my-5 text-center'>
+          <h1 className='display-5 text-center'>Welcome to the Texas Rate Finder!</h1>
+          <div className='col-lg-6 mx-auto'>
+            <p className='lead mb-4'>
+              Our mission is to help Texas Residential customers select the best plan for them.  
+              We believe that any customer should be able to select the best plan without spending hourse reading how each individual plan is different from the hundreds of other plans.  
+            </p>
+            <p className='lead mb-4'>
+              We appreciate you giving us an opportunity to help you find the best rate.  
+            </p>
+            <div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
+              <LearnMore />
+              
+              <button className='btn btn-success btn-lg px-4 gap-3' type='button'><b>Get Started</b></button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Finder;
--- src/Finder.test.js
+++ src/Finder.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Finder from './Finder';
 
 test('renders learn react link', () => {
   render(<App />);
