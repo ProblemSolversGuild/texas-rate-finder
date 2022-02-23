45170a8799400c37bf84b03d3b25c010342d0b4c
Kevin Bird <kevinbird15@gmail.com>
2022-02-22 23:14:49 -0600

    Switching to hashrouter to fix refresh issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 // import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
@@ -20,7 +20,7 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Auth0Provider
         domain={domain}
         clientId={clientId}
@@ -33,7 +33,7 @@ ReactDOM.render(
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Auth0Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
