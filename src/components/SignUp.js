82793084c5b398cb6bf3e9ae89e177d02ae7c169
Kevin Bird <kevinbird15@gmail.com>
2022-04-20 21:11:25 -0500

    disable email

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -41,7 +41,7 @@ function SignUp() {
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" defaultValue={user?user.email:null} onChange={(e) => setEmail(e.target.value)} /> 
+        <Form.Control disabled type="email"  placeholder="Enter email" defaultValue={user?user.email:null} onChange={(e) => setEmail(e.target.value)} /> 
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
