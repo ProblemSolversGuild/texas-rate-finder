0c01c711f31bf423440976daa4a527610651222a
Kevin Bird <kevinbird15@gmail.com>
2022-07-27 22:10:05 -0500

    updating usage charts

--- src/FinderMain.js
+++ src/FinderMain.js
@@ -64,23 +64,24 @@ function FinderMain() {
           </Offcanvas>
 
           <Col xs={12} lg={12}>
-            <Row className='py-5'></Row>
+            <Row className='py-2'></Row>
             <Row>
               <Col></Col>
               <Col></Col>
               <Col>
+
+              </Col>
+            </Row>
+            <Row style={{height:'30rem'}}>
+              <Col ></Col>
+              <Col xl={9} xxl={6}>
+                <UsageChart usageData={usage} />
+                {userInfo && ratePlanList && userInfo.esiid && <Button className="mx-0" variant="primary" onClick={handleShow}>{show?"Hide Plan Filters":"Show Plan Filters"}</Button>}
+                {(isAuthenticated)&&<Button className="mx-2" variant="primary" href='https://donate.stripe.com/eVa16r91H0E21HOdQS' target="_blank">Donate</Button>}                
                 {(userInfo && !userInfo.esiid && isAuthenticated)&&<Button variant="success" onClick={() => navigate('/SignUp')}>Connect Your Real Usage</Button>}
                 {(!isAuthenticated)&&<Button variant="success" onClick={() => {loginWithRedirect({ screen_hint: "signup", redirectUri: uri_front+'#/SignUp'})}}>Create an Account!</Button>}
               </Col>
-            </Row>
-            <Row>
-              <Col ></Col>
-              <Col xl={9} xxl={6}>
-              <UsageChart usageData={usage} />
-                {userInfo && ratePlanList && userInfo.esiid && <Button className="mx-0" variant="primary" onClick={handleShow}>{show?"Hide Plan Filters":"Show Plan Filters"}</Button>}
-                {(isAuthenticated)&&<Button className="mx-2" variant="primary" href='https://donate.stripe.com/eVa16r91H0E21HOdQS' target="_blank">Donate</Button>}
-              </Col>
-              <Col ></Col>
+              <Col></Col>
             </Row>
             <Row className='mt-1'>
               <Col></Col>
--- src/components/UsageChart.js
+++ src/components/UsageChart.js
@@ -2,10 +2,8 @@ import {LineChart, XAxis, YAxis, Line, Tooltip, ResponsiveContainer, Label} from
 
 const UsageChart = ({ usageData }) => {
     return (
-        <ResponsiveContainer className={'align-items-center'} width="100%" height="90%">
+        <ResponsiveContainer className={'align-items-center'} width={'100%'} height={'85%'}>
             <LineChart
-                width={1000}
-                height={500}
                 data={usageData}
                 >
                 <XAxis angle={-10} dataKey={'x'} tickFormatter={d => new Date(d).toLocaleDateString()}/>
