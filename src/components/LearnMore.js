d72f7d7198c2526cfeec1e0a3565e2a029504b36
Kevin Bird <kevinbird15@gmail.com>
2022-07-31 17:54:42 -0500

    push changes to landing page

--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -14,25 +14,24 @@ const LearnMore = () => {
 
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            {/* <Modal.Title>Learn More</Modal.Title> */}
           </Modal.Header>
           <Modal.Body>
             <h5>Who are we?</h5>
             <p>
-              We are <a href='https://problemsolversguild.com' target='_blank'>The Problem Solvers Guild</a>.  We identify problems that impact us and work to develop solutions through an iterative process of trial and error.
+              We are <a href='https://problemsolversguild.com' target='_blank'>The Problem Solvers Guild</a>.  We identify problems that impact us and work to develop solutions through an iterative process of trial and error.  If you have an application that you would like The Problem Solvers Guild to work on, we would love to hear from you!
             </p>
             <h5>Who pays us?</h5>
             <p>
-              We currently offer our service directly to residential customers and allow our customers to <a href='https://donate.stripe.com/eVa16r91H0E21HOdQS' target="_blank">donate to us</a>.<br/><br/>
-              For REPs or other companies that want to use our powerful rate calculator engine or run an analysis on all of your current rate plans, reach out to kevin@theproblemsolversguild.com.
+              Our tool is 100% donation supported. If the rate finder helped you find a new electric plan and you want it to continue to be free and available to all Texas customers, <a href='https://donate.stripe.com/eVa16r91H0E21HOdQS' target="_blank">donate to us</a>.<br/><br/>
+              If any REPs want to use the powerful engine we've built on their site to help customers find a plan that fits their usage, reach out to kevin@theproblemsolversguild.com.
             </p>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>Close</Button>
-            <Button variant="success" onClick={() => navigate("/app")}>Get Started</Button>
+            <Button variant="success" onClick={() => navigate("/app")}>View the Rate Finder</Button>
           </Modal.Footer>
         </Modal>
-        <button className='btn btn-success btn-lg px-4 gap-3' type='button'  onClick={() => navigate("/app")}><b>Get Started</b></button>
+        <button className='btn btn-success btn-lg px-4 gap-3' type='button'  onClick={() => navigate("/app")}><b>View the Rate Finder</b></button>
       </>
     );
   }
